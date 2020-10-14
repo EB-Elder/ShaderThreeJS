d720dc38ad0006c94154b3b448b0b120fd3b4e64
elhajjam.abdallah1@gmail.com <elhajjam.abdallah1@gmail.com>
2020-10-14 17:49:05 +0200

    First Shader done

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,43 +1,56 @@
+
+//Instanciation du moteur de rendu
+var renderer = new THREE.WebGLRenderer();
+
+//Instanciation de la scene
 var scene = new THREE.Scene();
+
+//Instanciation de la caméra
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-/* var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF, wireframe: true});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube); */
+var clock = 0;
 
-var geometry = new THREE.Geometry();
-var material = new THREE.MeshBasicMaterial({color:0x00ff00 });
-geometry.vertices.push(
-	new THREE.Vector3( -10,  10, 0 ),
-	new THREE.Vector3( -10, -10, 0 ),
-	new THREE.Vector3(  10, -10, 0 )
-);
+var origin = new THREE.Vector3(0, 0, 0);
 
-geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
+var uniforms;
 
-geometry.computeBoundingSphere();
-
-var mesh = new THREE.Mesh(geometry, material);
+var initScene = function()
+{
+    THREE.WebGLProgram
+    //Initialisation de la scene
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
 
+    uniforms = {
+        "time": {value: clock}
+    }
+    
+    var shader = new THREE.ShaderMaterial({
+        uniforms: uniforms,
+        vertexShader: document.getElementById("vertexShader").textContent,
+        fragmentShader: document.getElementById("fragmentShader").textContent
+        
+        
+    })
+    var geometry = new THREE.BoxGeometry(10, 10, 10);
+    var cube = new THREE.Mesh(geometry, shader);
+    scene.add(cube);
+}
 
-camera.position.z = 50;
 
-scene.add(mesh);
-var time = 0;
 var update = function()
 {
-    /* camera.lookAt(new THREE.Vector3(0,0,0));
-    camera.position.x = Math.cos(time)*50
-    camera.position.z = Math.sin(time)*50
-    time+=0.01 */
+    
+    camera.position.z = 20;
+    scene.rotateX(Math.cos(clock)/200)
+    scene.rotateZ(Math.sin(clock)/200)
+    clock = performance.now()/2000;
+    uniforms["time"].value = clock*50;
 };
 
+//Fonction qui va permettre de rendre la scene avec la camera voulu
 var render = function()
 {
     renderer.render(scene, camera);
@@ -51,4 +64,5 @@ var GameLoop = function()
     render();
 };
 
+initScene();
 GameLoop();
